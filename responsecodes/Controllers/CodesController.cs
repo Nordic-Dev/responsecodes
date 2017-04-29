c5d60029b564521046ce2bb0b1b4a2c5e38d97b6
Alec Papierniak <alec@papierniak.net>
2017-04-29 14:12:16 -0500

    Updating routes

--- responsecodes/Controllers/CodesController.cs
+++ responsecodes/Controllers/CodesController.cs
@@ -7,18 +7,22 @@ using Newtonsoft.Json;
 
 namespace responsecodes.Controllers
 {
+    [Route("/")]
     public class CodesController : Controller
     {
+        [Route("four")]
         public IActionResult Four()
         {
             return BadRequest(JsonConvert.SerializeObject("Bad request, yo!"));
         }
 
+        [Route("two")]
         public IActionResult Two()
         {
             return new CreatedResult("/here", JsonConvert.SerializeObject("wellDone"));
         }
 
+        [Route("five")]
         public IActionResult Five()
         {
             return StatusCode(500, JsonConvert.SerializeObject("Big failure!"));
